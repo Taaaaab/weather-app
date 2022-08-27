c97f78124c0b40cb9d6571a05f03351a69228232
Taaaaab <owen.whitsett@gmail.com>
2022-08-27 13:34:19 -0400

    Return weather object and display on page

--- script.js
+++ script.js
@@ -3,8 +3,6 @@ const input = document.querySelector('input');
 const searchBtn = document.querySelector('.btn');
 const display = document.querySelector('.display');
 
-// display.textContent = weatherData.data.images.original.url;
-
 form.addEventListener('submit', (event) => {
     event.preventDefault();
     async function getWeather() {
@@ -12,6 +10,21 @@ form.addEventListener('submit', (event) => {
     const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=076885a9fd9b3c3417323cdbba5f8cbf`, {mode: 'cors'})     
     const weatherData = await response.json(); 
     console.log(weatherData);
+
+    const weatherObject = getWeatherObject(weatherData);
+    display.textContent = weatherObject.name + " " +
+    weatherObject.description + " " +
+    weatherObject.high + " " +
+    weatherObject.low;
     }
     getWeather();
-})
+
+    function getWeatherObject(weather) {
+        const name = weather.name;
+        const temp = weather.main.temp;
+        const description = weather.weather[0].description;
+        const high = weather.main.temp_max;
+        const low = weather.main.temp_min;
+        return {name, temp, description, high, low};
+    }
+});
