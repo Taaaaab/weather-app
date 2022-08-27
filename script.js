32052b7a871a64f46f478872b73a780a6f4a8710
Taaaaab <owen.whitsett@gmail.com>
2022-08-27 12:31:33 -0400

    Working open weather API with search

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
+const form = document.getElementsByTagName('form')[0];
 const input = document.querySelector('input');
 const searchBtn = document.querySelector('.btn');
 const display = document.querySelector('.display');
 
-async function getWeather() {
-    const response = await fetch('', {mode: 'cors'})     
+// display.textContent = weatherData.data.images.original.url;
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    async function getWeather() {
+    let search = document.getElementById('search').value;
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=076885a9fd9b3c3417323cdbba5f8cbf`, {mode: 'cors'})     
     const weatherData = await response.json(); 
-    display.textContent = weatherData.data.images.original.url;
-}
-getWeather();
+    console.log(weatherData);
+    }
+    getWeather();
+})
