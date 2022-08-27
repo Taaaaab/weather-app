fcbf2ae48cbbc3ec7c8c126bc0111f6b284c6d74
Taaaaab <owen.whitsett@gmail.com>
2022-08-27 16:14:48 -0400

    API Call appears on webpage with updated stylesheet

--- script.js
+++ script.js
@@ -1,21 +1,23 @@
 const form = document.getElementsByTagName('form')[0];
 const input = document.querySelector('input');
 const searchBtn = document.querySelector('.btn');
-const display = document.querySelector('.display');
+const display = document.querySelector('.display-body');
+const city = document.querySelector('.display-header');
 
 form.addEventListener('submit', (event) => {
     event.preventDefault();
     async function getWeather() {
     let search = document.getElementById('search').value;
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=076885a9fd9b3c3417323cdbba5f8cbf`, {mode: 'cors'})     
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=076885a9fd9b3c3417323cdbba5f8cbf&units=imperial`, {mode: 'cors'})     
     const weatherData = await response.json(); 
     console.log(weatherData);
 
     const weatherObject = getWeatherObject(weatherData);
-    display.textContent = weatherObject.name + " " +
-    weatherObject.description + " " +
-    weatherObject.high + " " +
-    weatherObject.low;
+    city.textContent = weatherObject.name;
+    display.innerHTML = ("Current Temp: " + weatherObject.temp + '<br/>' +
+    weatherObject.description + '<br/>' +
+    "High: " + weatherObject.high + '<br/>' +
+    "Low: " + weatherObject.low);
     }
     getWeather();
 
